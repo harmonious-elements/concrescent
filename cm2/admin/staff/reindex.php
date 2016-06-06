2c9c6d5b5d6282702984379cf8fe5582a805bf44
Rebecca G. Bettencourt <beckiejo@gmail.com>
2016-06-05 21:54:34 -0700

    Factoring out reindex css and js

--- cm2/admin/staff/reindex.php
+++ cm2/admin/staff/reindex.php
@@ -4,12 +4,12 @@ ini_set('max_execution_time', 300);
 ini_set('memory_limit', '1024M');
 
 require_once dirname(__FILE__).'/../../lib/database/staff.php';
+require_once dirname(__FILE__).'/../../lib/util/res.php';
 require_once dirname(__FILE__).'/../admin.php';
-
-$sdb = new cm_staff_db($db);
 
 if (isset($_POST['action'])) {
 	header('Content-type: text/plain');
+	$sdb = new cm_staff_db($db);
 	if ($_POST['action'] == 'init') {
 		$_SESSION['staff_reindex_time'] = microtime(true);
 		$_SESSION['staff_reindex_entities'] = $sdb->list_staff_members();
@@ -45,113 +45,7 @@ if (isset($_POST['action'])) {
 }
 
 cm_admin_head('Rebuild Staff Search Index');
-
-?><style>
-	.progress-track {
-		display: inline-block;
-		position: relative;
-		width: 200px;
-		height: 12px;
-		background: #ccf;
-		border: solid 1px black;
-		margin: -3px 2em -3px 2em;
-	}
-	.progress-bar {
-		display: block;
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 0;
-		bottom: 0;
-		background: #444;
-	}
-</style><?php
-
-?><script>
-	(function($,window,document,cmui){
-		var doAjax = function(request, done) {
-			window.setTimeout(function() {
-				$.post('reindex.php', request, function(response) {
-					if (!response['ok']) {
-						cmui.showButterbarPersistent('An error occurred. Please try again.');
-					} else {
-						var progress = Math.floor(100 * response['done'] / response['total']) + '%';
-						var time = Math.round(100 * response['time']) / 100;
-						$('.progress-bar').css('width', progress);
-						$('.progress-label').text(progress);
-						$('.time-label').text(time);
-						window.setTimeout(function() {
-							done(response);
-						}, 10);
-					}
-				}, 'json');
-			}, 10);
-		};
-		var setStatus = function(inProgress, statusLabel, showProgress) {
-			if (inProgress) {
-				$('.status-label').text(statusLabel);
-				if (showProgress) {
-					$('.progress-track').removeClass('hidden');
-					$('.progress-label').removeClass('hidden');
-				} else {
-					$('.progress-track').addClass('hidden');
-					$('.progress-label').addClass('hidden');
-				}
-				$('.cm-warning-box').addClass('hidden');
-				$('.cm-error-box').removeClass('hidden');
-				$('.cm-note-box').removeClass('hidden');
-				$('.cm-success-box').addClass('hidden');
-			} else {
-				$('.cm-warning-box').removeClass('hidden');
-				$('.cm-error-box').addClass('hidden');
-				$('.cm-note-box').addClass('hidden');
-				$('.cm-success-box').removeClass('hidden');
-			}
-		};
-
-		var reindexInit, reindexDrop, reindexIndex, reindexDone;
-		reindexInit = function() {
-			$('button').unbind('click');
-			$('button').prop('disabled', true);
-			$(window).bind('beforeunload', function() {
-				return 'Reindexing is still in progress. Leaving this page will result in a partial index.';
-			});
-			setStatus(true, 'Starting...', false);
-			doAjax({'action': 'init'}, function(response) {
-				reindexDrop();
-			});
-		};
-		reindexDrop = function() {
-			setStatus(true, 'Dropping...', false);
-			doAjax({'action': 'drop'}, function(response) {
-				reindexIndex(0, 50);
-			});
-		};
-		reindexIndex = function(offset, length) {
-			setStatus(true, 'Indexing...', true);
-			doAjax({'action': 'index', 'offset': offset, 'length': length}, function(response) {
-				if ((1 * response['done']) >= (1 * response['total'])) {
-					reindexDone();
-				} else {
-					reindexIndex(offset + length, length);
-				}
-			});
-		};
-		reindexDone = function() {
-			setStatus(true, 'Finishing...', false);
-			doAjax({'action': 'done'}, function(response) {
-				setStatus(false, null, false);
-				$(window).unbind('beforeunload');
-				$('button').prop('disabled', false);
-				$('button').bind('click', reindexInit);
-			});
-		};
-
-		$(document).ready(function() {
-			$('button').bind('click', reindexInit);
-		});
-	})(jQuery,window,document,cmui);
-</script><?php
+echo '<script type="text/javascript" src="' . htmlspecialchars(resource_file_url('cmreindex.js', false)) . '"></script>';
 
 cm_admin_body('Rebuild Staff Search Index');
 cm_admin_nav('staff-reindex');
---
+++ cm2/lib/res/cmreindex.js
@@ -0,0 +1,88 @@
+(function($,window,document,cmui){
+	var doAjax = function(request, done) {
+		window.setTimeout(function() {
+			$.post('reindex.php', request, function(response) {
+				if (!response['ok']) {
+					cmui.showButterbarPersistent('An error occurred. Please try again.');
+				} else {
+					var progress = Math.floor(100 * response['done'] / response['total']) + '%';
+					var time = Math.round(100 * response['time']) / 100;
+					$('.progress-bar').css('width', progress);
+					$('.progress-label').text(progress);
+					$('.time-label').text(time);
+					window.setTimeout(function() {
+						done(response);
+					}, 10);
+				}
+			}, 'json');
+		}, 10);
+	};
+
+	var setStatus = function(inProgress, statusLabel, showProgress) {
+		if (inProgress) {
+			$('.status-label').text(statusLabel);
+			if (showProgress) {
+				$('.progress-track').removeClass('hidden');
+				$('.progress-label').removeClass('hidden');
+			} else {
+				$('.progress-track').addClass('hidden');
+				$('.progress-label').addClass('hidden');
+			}
+			$('.cm-warning-box').addClass('hidden');
+			$('.cm-error-box').removeClass('hidden');
+			$('.cm-note-box').removeClass('hidden');
+			$('.cm-success-box').addClass('hidden');
+		} else {
+			$('.cm-warning-box').removeClass('hidden');
+			$('.cm-error-box').addClass('hidden');
+			$('.cm-note-box').addClass('hidden');
+			$('.cm-success-box').removeClass('hidden');
+		}
+	};
+
+	var reindexInit, reindexDrop, reindexIndex, reindexDone;
+	reindexInit = function() {
+		$('button').unbind('click');
+		$('button').prop('disabled', true);
+		$(window).bind('beforeunload', function() {
+			return 'Reindexing is still in progress. Leaving this page will result in a partial index.';
+		});
+		setStatus(true, 'Starting...', false);
+		doAjax({'action': 'init'}, function(response) {
+			reindexDrop();
+		});
+	};
+	reindexDrop = function() {
+		setStatus(true, 'Dropping...', false);
+		doAjax({'action': 'drop'}, function(response) {
+			reindexIndex(0, 50);
+		});
+	};
+	reindexIndex = function(offset, length) {
+		setStatus(true, 'Indexing...', true);
+		doAjax({
+			'action': 'index',
+			'offset': offset,
+			'length': length
+		}, function(response) {
+			if ((1 * response['done']) >= (1 * response['total'])) {
+				reindexDone();
+			} else {
+				reindexIndex(offset + length, length);
+			}
+		});
+	};
+	reindexDone = function() {
+		setStatus(true, 'Finishing...', false);
+		doAjax({'action': 'done'}, function(response) {
+			setStatus(false, null, false);
+			$(window).unbind('beforeunload');
+			$('button').prop('disabled', false);
+			$('button').bind('click', reindexInit);
+		});
+	};
+
+	$(document).ready(function() {
+		$('button').bind('click', reindexInit);
+	});
+})(jQuery,window,document,cmui);
