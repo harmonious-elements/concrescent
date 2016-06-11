afb7f354ece6b53f4e312418a680a8d1b31ca7ed
Rebecca G. Bettencourt <beckiejo@gmail.com>
2016-06-11 12:34:13 -0700

    Staff statistics

--- cm2/admin/statistics.php
+++ cm2/admin/statistics.php
@@ -1,47 +1,92 @@
 <?php
 
 require_once dirname(__FILE__).'/../lib/database/attendee.php';
+require_once dirname(__FILE__).'/../lib/database/staff.php';
 require_once dirname(__FILE__).'/admin.php';
 
 cm_admin_check_permission('statistics', 'statistics');
 
-$atdb = new cm_attendee_db($db);
-$name_map = $atdb->get_badge_type_name_map();
-$atstat = $atdb->get_attendee_statistics(300, $name_map);
-$name_map['*'] = 'Attendees';
-
-$header = array('Date');
-$last = array(0);
-foreach ($name_map as $btid => $btname) {
-	$header[] = $btname . ' - In System';
-	$header[] = $btname . ' - Paid';
-	$header[] = $btname . ' - Printed';
-	$header[] = $btname . ' - Checked In';
-	$last[] = 0;
-	$last[] = 0;
-	$last[] = 0;
-	$last[] = 0;
+function atsection($db) {
+	$atdb = new cm_attendee_db($db);
+	$atnames = $atdb->get_badge_type_name_map();
+	$atstat = $atdb->get_attendee_statistics(300, $atnames);
+	$atnames['*'] = 'All Attendees';
+	return array('names' => $atnames, 'stat' => $atstat);
 }
 
-$data = array($header);
-$timestamps = $atstat['timestamps'];
+function ssection($db) {
+	$sdb = new cm_staff_db($db);
+	$snames = $sdb->get_badge_type_name_map();
+	$sstat = $sdb->get_staff_statistics(300, $snames);
+	$snames['*'] = 'All Staff';
+	return array('names' => $snames, 'stat' => $sstat);
+}
+
+$sections = array(
+	atsection($db),
+	ssection($db),
+);
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+$columns = array('In System', 'Paid', 'Printed', 'Checked In');
+
+$timestamps = array();
+$header = array('Date');
+$last = array(0);
+foreach ($sections as $section) {
+	$timestamps += $section['stat']['timestamps'];
+	foreach ($section['names'] as $name) {
+		foreach ($columns as $col) {
+			$header[] = $name . ' - ' . $col;
+			$last[] = 0;
+		}
+	}
+}
+foreach ($columns as $col) {
+	$header[] = 'Grand Total - ' . $col;
+	$last[] = 0;
+}
 ksort($timestamps);
+
+$data = array($header);
 foreach ($timestamps as $timestamp) {
 	$row = array($timestamp);
-	foreach ($name_map as $btid => $btname) {
-		$row[] = isset($atstat['timelines'][$btid][0][$timestamp]) ? ($last[count($row)] = $atstat['timelines'][$btid][0][$timestamp]) : $last[count($row)];
-		$row[] = isset($atstat['timelines'][$btid][1][$timestamp]) ? ($last[count($row)] = $atstat['timelines'][$btid][1][$timestamp]) : $last[count($row)];
-		$row[] = isset($atstat['timelines'][$btid][2][$timestamp]) ? ($last[count($row)] = $atstat['timelines'][$btid][2][$timestamp]) : $last[count($row)];
-		$row[] = isset($atstat['timelines'][$btid][3][$timestamp]) ? ($last[count($row)] = $atstat['timelines'][$btid][3][$timestamp]) : $last[count($row)];
+	$totals = array();
+	foreach ($columns as $idx => $col) {
+		$totals[$idx] = 0;
+	}
+	foreach ($sections as $section) {
+		$timelines = $section['stat']['timelines'];
+		foreach ($section['names'] as $id => $name) {
+			foreach ($columns as $idx => $col) {
+				$index = count($row);
+				if (isset($timelines[$id][$idx][$timestamp])) {
+					$last[$index] = $timelines[$id][$idx][$timestamp];
+				}
+				if ($id == '*') {
+					$totals[$idx] += $last[$index];
+				}
+				$row[] = $last[$index];
+			}
+		}
+	}
+	foreach ($columns as $idx => $col) {
+		$row[] = $totals[$idx];
 	}
 	$data[] = $row;
 }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 cm_admin_head('Statistics');
 if (count($header) > 1 && count($data) > 1) {
 	$json_data = json_encode($data);
 	$json_data = preg_replace('/\\[([0-9]+)/', '[new Date($1)', $json_data);
-	echo '<script type="text/javascript">cm_stat_data = (' . $json_data . '); cm_stat_count = ' . count($header) . ';</script>';
+	echo '<script type="text/javascript">';
+	echo 'cm_stat_data = (' . $json_data . '); ';
+	echo 'cm_stat_count = ' . count($header) . ';';
+	echo '</script>';
 	echo '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>';
 	echo '<script type="text/javascript" src="statistics.js"></script>';
 }
@@ -56,29 +101,46 @@ echo '<article>';
 				echo '<div class="spacing"><div id="cm-stat-chart">Loading Chart...</div></div>';
 			}
 			echo '<table border="0" cellpadding="0" cellspacing="0" class="cm-statistics">';
-				echo '<thead>';
-					echo '<tr>';
-						echo '<th class="cm-stat-corner">Badge Type</th>';
-						echo '<th class="td-numeric cm-stat-col" id="cm-stat-col-1">In System</th>';
-						echo '<th class="td-numeric cm-stat-col" id="cm-stat-col-2">Paid</th>';
-						echo '<th class="td-numeric cm-stat-col" id="cm-stat-col-3">Printed</th>';
-						echo '<th class="td-numeric cm-stat-col" id="cm-stat-col-4">Checked In</th>';
-					echo '</tr>';
-				echo '</thead>';
+				echo '<tr>';
+					echo '<th class="cm-stat-corner">Badge Type</th>';
+					foreach ($columns as $idx => $col) {
+						echo '<th class="td-numeric cm-stat-col" id="cm-stat-col-'.($idx+1).'">';
+						echo htmlspecialchars($col);
+						echo '</th>';
+					}
+				echo '</tr>';
 				$index = 0;
-				foreach ($atstat['counters'] as $btid => $count) {
-					$tbody = ($btid == '*') ? 'tfoot' : 'tbody';
-					echo '<'.$tbody.'>';
-						echo '<tr>';
-							$td = ($btid == '*') ? 'th' : 'td';
-							echo '<'.$td.' class="cm-stat-row" id="cm-stat-row-' . ($index + 1) . '">' . htmlspecialchars($name_map[$btid]) . '</'.$td.'>';
-							echo '<'.$td.' class="td-numeric cm-stat" id="cm-stat-' . (++$index) . '">' . number_format($count[0]) . '</'.$td.'>';
-							echo '<'.$td.' class="td-numeric cm-stat" id="cm-stat-' . (++$index) . '">' . number_format($count[1]) . '</'.$td.'>';
-							echo '<'.$td.' class="td-numeric cm-stat" id="cm-stat-' . (++$index) . '">' . number_format($count[2]) . '</'.$td.'>';
-							echo '<'.$td.' class="td-numeric cm-stat" id="cm-stat-' . (++$index) . '">' . number_format($count[3]) . '</'.$td.'>';
-						echo '</tr>';
-					echo '</'.$tbody.'>';
+				$totals = array();
+				foreach ($columns as $idx => $col) {
+					$totals[$idx] = 0;
 				}
+				foreach ($sections as $section) {
+					$counters = $section['stat']['counters'];
+					foreach ($section['names'] as $id => $name) {
+						echo '<tr>';
+							$td = ($id == '*') ? 'th' : 'td';
+							echo '<'.$td.' class="cm-stat-row" id="cm-stat-row-'.($index+1).'">';
+							echo htmlspecialchars($name);
+							echo '</'.$td.'>';
+							foreach ($columns as $idx => $col) {
+								echo '<'.$td.' class="td-numeric cm-stat" id="cm-stat-'.(++$index).'">';
+								echo number_format($counters[$id][$idx]);
+								echo '</'.$td.'>';
+								if ($id == '*') {
+									$totals[$idx] += $counters[$id][$idx];
+								}
+							}
+						echo '</tr>';
+					}
+				}
+				echo '<tr>';
+					echo '<th class="cm-stat-row" id="cm-stat-row-'.($index+1).'">Grand Total</th>';
+					foreach ($columns as $idx => $col) {
+						echo '<th class="td-numeric cm-stat" id="cm-stat-'.(++$index).'">';
+						echo number_format($totals[$idx]);
+						echo '</th>';
+					}
+				echo '</tr>';
 			echo '</table>';
 		echo '</div>';
 	echo '</div>';
